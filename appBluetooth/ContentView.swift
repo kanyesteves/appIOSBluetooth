9c1c620aa52c86c8bf37335a60bf2e1c219f186f
kanyesteves <kany.esteves@gmail.com>
2024-04-15 19:29:43 -0300

    Initial Commit

---
+++ appBluetooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  appBluetooth
+//
+//  Created by Kanydian Esteves on 15/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ appBluetooth/appBluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  appBluetoothApp.swift
+//  appBluetooth
+//
+//  Created by Kanydian Esteves on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct appBluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
